5d040767488b501e7d30c31d91c309826cd886ae
Ruslan Latypov <rus_91.08@mail.ru>
2018-05-05 13:29:39 +0300

    updated user->follows schema

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-var userRef = new Schema({ name: String });
-
 const userSchema = new Schema({
     name: String,
-    follows: [userRef],
-    followers: [userRef],
+    follows: [String],
     avatar: String
 });
 
